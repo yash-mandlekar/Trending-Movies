b688e49627dad7c00420690f25f9ef72393dfcaf
yash-mandlekar <ymandlekar9826@gmail.com>
2023-01-18 23:51:58 +0530

    updating

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      
+    <div className="navbar">
+      <nav>
+        <a href="#home" class="logo">
+          Netflix
+        </a>
+        <ul>
+          <li>
+            <a href="#browse">Browse</a>
+          </li>
+          <li>
+            <a href="#my-list">My List</a>
+          </li>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+        </ul>
+        <form>
+          <input type="search" placeholder="Search" />
+          <button>Search</button>
+        </form>
+        <ul>
+          <li>
+            <a href="#account">Account</a>
+          </li>
+          <li>
+            <a href="#help">Help</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
-}
+};
 
 export default Navbar;
